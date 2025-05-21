7638844a75a84603177f71dbeaff241e8b1985d5
Ricca665 <riccardosarti2010@gmail.com>
2025-05-21 18:44:19 +0200

    fixed version number

--- Setup.py
+++ Setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='BadAppleLib',
-    version='0.0.1', # Version
+    version='1.0.0', # Version
     description='A library specifically made to just play bad apple!',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
